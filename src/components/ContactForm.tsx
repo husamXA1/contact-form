41ca19fae9c5cefb29221b6bbee3891cf34bb96f
husamXA1 <husamug@gmail.com>
2024-05-25 07:06:40 +0200

    layout initialization

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,40 +5,56 @@ export default function ContactForm() {
         <div className="contact-form container p-4 rounded-4 my-5">
             <h2>Contact Us</h2>
 
-            <div className="fullname d-md-flex">
-                <label htmlFor="fname">First Name</label>
-                <input type="text" name="fname" id="fname" />
-                This field is required
+            <div className="fullname d-sm-flex gap-3">
+                <div className="fname">
+                    <label htmlFor="fname">First Name</label>
+                    <input type="text" name="fname" id="fname" />
+                    This field is required
+                </div>
+                <div className="lname">
+                    <label htmlFor="lname">Last Name</label>
+                    <input type="text" name="lname" id="lname" />
+                    This field is required
+                </div>
+            </div>
 
-                <label htmlFor="lname">Last Name</label>
-                <input type="text" name="lname" id="lname" />
+            <div className="email">
+                <label htmlFor="email">Email Address</label>
+                <input type="email" name="email" id="email" />
+                Please enter a valid email address
                 This field is required
             </div>
 
-            <label htmlFor="email">Email Address</label>
-            <input type="email" name="email" id="email" />
-            Please enter a valid email address
-            This field is required
+            <div className="query-type">
+                <label htmlFor="query_type">Query Type</label>
+                <div className="d-sm-flex gap-3">
+                    <div className="radio-btn w-100 border border-dark p-2 my-1">
+                        <input type="radio" name="query_type" id="general_query" />
+                        <label htmlFor="general_query">General Enquiry</label>
+                    </div>
+                    <div className="radio-btn w-100 border border-dark p-2 my-1">
+                        <input type="radio" name="query_type" id="support_query" />
+                        <label htmlFor="support_query">Support Request</label>
+                    </div>
+                </div>
+                Please select a query type
+            </div>
 
-            <label htmlFor="query_type">Query Type</label>
-            <input type="radio" name="query_type" id="general_query" />
-            <label htmlFor="general_query">General Enquiry</label>
-            <input type="radio" name="query_type" id="support_query" />
-            <label htmlFor="support_query">Support Request</label>
-            
-            Please select a query type
+            <div className="message">
+                <label htmlFor="message">Message</label>
+                <textarea className='w-100' name="message" id="message"></textarea>
+                This field is required
+            </div>
 
-            <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" cols={30} rows={10}></textarea>
-            This field is required
-
-            <input type="checkbox" name="consent" id="consent" />
-            <label htmlFor="consent">I consent to being contacted by the team</label>
-            To submit this form, please consent to being contacted
+            <div className="consent">
+                <input type="checkbox" name="consent" id="consent" />
+                <label htmlFor="consent">I consent to being contacted by the team</label>
+                To submit this form, please consent to being contacted
+            </div>
 
             <button className="btn w-100">Submit</button>
 
-            { false && <SuccessMessage /> }
+            {false && <SuccessMessage />}
 
         </div>
     )

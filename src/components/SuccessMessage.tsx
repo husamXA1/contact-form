c920f83379a13543b99fae3cd36eab4b4cddfe01
husamXA1 <husamug@gmail.com>
2024-05-28 07:59:58 +0200

    fixed success message covering inputs

--- src/components/SuccessMessage.tsx
+++ src/components/SuccessMessage.tsx
@@ -2,7 +2,7 @@ import successCheck from '../assets/images/icon-success-check.svg'
 
 export default function SuccessMessage({show}:{show : boolean}) {
     return (
-        <div className={`success-message ${show?"on":""}`}>
+        <div className={`success-message ${show?"on":""} d-flex flex-column justify-content-center`}>
             <p className='fs-5 mt-2'><img src={successCheck} alt="checkmark" className='me-2' /><b>Message Sent!</b></p>
             <p>Thanks for completing the form. We'll be in touch soon!</p>
         </div>
